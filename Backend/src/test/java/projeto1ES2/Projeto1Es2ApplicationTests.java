c9d88c42a9ab9718439d07c5a6d5a7bc3a6a3bcf
JPOrtegaa <joaopedro.ortega17@gmail.com>
2024-03-30 16:50:04 -0300

    feat: estruturar backend (camada controller, serviços, dao)

---
+++ Backend/src/test/java/projeto1ES2/Projeto1Es2ApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto1ES2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto1Es2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
