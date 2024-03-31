529239c886597372f8ba2ec2f2c7e136000e0bb1
Matheus A <matheus.andrade1122@gmail.com>
2024-03-31 17:27:16 -0300

    Adicionando dados ao dump

--- dump.sql
+++ dump.sql
@@ -61,7 +61,7 @@ CREATE TABLE `Profissional` (
   KEY `fk_Profissional_Raca1_idx` (`Raca_idRaca`),
   CONSTRAINT `fk_Profissional_Genero1` FOREIGN KEY (`Genero_idGenero`) REFERENCES `Genero` (`idGenero`),
   CONSTRAINT `fk_Profissional_Raca1` FOREIGN KEY (`Raca_idRaca`) REFERENCES `Raca` (`idRaca`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,8 +70,30 @@ CREATE TABLE `Profissional` (
 
 LOCK TABLES `Profissional` WRITE;
 /*!40000 ALTER TABLE `Profissional` DISABLE KEYS */;
-INSERT INTO `Profissional` VALUES (4,'Jose','2002-01-15','front-ender','Rua San Martin',5,2);
+-- INSERT INTO `Profissional` VALUES (4,'Jose','2002-01-15','front-ender','Rua San Martin',5,2);
 /*!40000 ALTER TABLE `Profissional` ENABLE KEYS */;
+
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Horton Ainslee', '1996-02-13', 'Designer', '77 Northview Pass', 3, 5);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Bonnibelle Giovannini', '1991-03-16', 'Administrador de banco de dados', '9761 Rowland Lane', 4, 1);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Maureen MacNeilly', '2000-09-04', 'Administrador de banco de dados', '83 Dwight Way', 3, 2);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Nanice Normavell', '2000-09-23', 'Desenvolvedor', '2 Village Lane', 3, 3);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Emelen Winkle', '1998-06-06', 'Desenvolvedor', '474 Ryan Place', 4, 2);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Halette Bulbeck', '1986-06-30', 'Designer', '043 East Road', 4, 3);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Querida Randals', '1999-04-23', 'Outro', '9 Stang Drive', 4, 5);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Donny Sabater', '2001-05-09', 'Analista de Sistemas', '78170 Porter Avenue', 3, 4);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Abbye Strooband', '1992-07-26', 'Outro', '77024 Mcguire Way', 3, 3);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Weider Rudwell', '1985-02-01', 'Administrador de banco de dados', '395 Northridge Pass', 3, 4);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Quintin Fairall', '1989-11-19', 'Outro', '689 Lyons Circle', 5, 1);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Kaja Ivanyushin', '1994-09-23', 'Outro', '01 Elgar Street', 4, 3);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Lurleen MacCarlich', '1994-09-19', 'Desenvolvedor', '80 Grim Plaza', 4, 5);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Hatty Kynastone', '1981-02-04', 'Designer', '4 Rowland Park', 5, 2);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Leighton Filler', '1997-03-21', 'Outro', '81 Fairfield Avenue', 5, 4);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Arri Roden', '1989-09-06', 'Desenvolvedor', '63804 Merrick Road', 3, 4);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Ingeberg Dunnet', '1982-08-30', 'Designer', '94525 Center Alley', 4, 3);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Talbert Gallear', '1980-05-13', 'Designer', '5733 Loftsgordon Parkway', 5, 3);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Silvain Gorrissen', '1992-12-14', 'Designer', '6334 Ridgeview Pass', 3, 3);
+insert into Profissional (nomeProfissional, dataNascimento, especialidadeProfissional, enderecoProfissional, Genero_idGenero, Raca_idRaca) values ('Dionysus Ladd', '1993-04-30', 'Outro', '08 Kedzie Point', 4, 4);
+
 UNLOCK TABLES;
 
 --
@@ -83,9 +105,9 @@ DROP TABLE IF EXISTS `Projeto`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Projeto` (
   `idProjeto` int NOT NULL AUTO_INCREMENT,
-  `nomeProjeto` varchar(45) NOT NULL,
-  `nomeCliente` varchar(45) NOT NULL,
-  `objetivoProjeto` varchar(45) NOT NULL,
+  `nomeProjeto` varchar(100) NOT NULL,
+  `nomeCliente` varchar(100) NOT NULL,
+  `objetivoProjeto` varchar(500) NOT NULL,
   `dataInicio` date NOT NULL,
   `dataTermino` date NOT NULL,
   `valorProjeto` float NOT NULL,
@@ -94,7 +116,7 @@ CREATE TABLE `Projeto` (
   UNIQUE KEY `idProjeto_UNIQUE` (`idProjeto`),
   KEY `fk_Projeto_Time_idx` (`Time_idTime`),
   CONSTRAINT `fk_Projeto_Time` FOREIGN KEY (`Time_idTime`) REFERENCES `Time` (`idTime`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,7 +125,7 @@ CREATE TABLE `Projeto` (
 
 LOCK TABLES `Projeto` WRITE;
 /*!40000 ALTER TABLE `Projeto` DISABLE KEYS */;
-INSERT INTO `Projeto` VALUES (5,'projeto1','cliente1','objetivo1','2024-08-15','2024-09-20',425.22,8),(7,'projeto2','cliente2','objetivo3','2024-08-12','2024-09-21',425.55,12);
+-- INSERT INTO `Projeto` VALUES (5,'projeto1','cliente1','objetivo1','2024-08-15','2024-09-20',425.22,8),(7,'projeto2','cliente2','objetivo3','2024-08-12','2024-09-21',425.55,12);
 /*!40000 ALTER TABLE `Projeto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -144,7 +166,7 @@ CREATE TABLE `Time` (
   `nomeTime` varchar(45) NOT NULL,
   PRIMARY KEY (`idTime`),
   UNIQUE KEY `idTime_UNIQUE` (`idTime`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -153,7 +175,7 @@ CREATE TABLE `Time` (
 
 LOCK TABLES `Time` WRITE;
 /*!40000 ALTER TABLE `Time` DISABLE KEYS */;
-INSERT INTO `Time` VALUES (8,'Sem Time'),(12,'time1');
+INSERT INTO `Time` VALUES (8,'Sem Time');
 /*!40000 ALTER TABLE `Time` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -181,7 +203,7 @@ CREATE TABLE `Time_Profissional` (
 
 LOCK TABLES `Time_Profissional` WRITE;
 /*!40000 ALTER TABLE `Time_Profissional` DISABLE KEYS */;
-INSERT INTO `Time_Profissional` VALUES (12,4);
+-- INSERT INTO `Time_Profissional` VALUES (12,4);
 /*!40000 ALTER TABLE `Time_Profissional` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
